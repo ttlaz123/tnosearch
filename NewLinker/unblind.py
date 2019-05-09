8453e5a140351e6eb9c2b393f30ca7266a3d4a80
ttlaz123 <tongtianliu@gmail.com>
2019-05-09 09:58:37 -0700

    making fakes

--- NewLinker/unblind.py
+++ NewLinker/unblind.py
@@ -6,10 +6,11 @@ sys.path.insert(0, tnopath)
 import argparse
 import pickle
 from astropy.table import Table
+import pandas as pd
 
 from LinkerLib import writeTriplets
 from LinkerLib import pickleTriplets
-import LinkerLib
+import LinkerLib as LL
 
 
 def makeFakeDict(fakesFile):
@@ -34,11 +35,59 @@ def unblindTrips(triplets, fakeDict):
             det.fakeid = fakeDict[det.objid]
     return triplets
 
+
+def unblindPatch(fakeDict, patchNumber):
+    patch = "{0:03d}".format(patchNumber)
+    df = pd.read_csv('ccdRM+SNFIT_SEASON' + patch + '_ML06.csv')
+    df = unblindDetections(df, fakeDict)
+    df.to_csv('unblinded+SNFIT_SEASON' + patch + '_ML06.csv')
+
+
+def unblindDetections(df, fakeDict):
+    objids = df['objid'].tolist()
+    fakeids = []
+    for objid in objids:
+        fakeids.append(fakeDict[objid])
+    df['fakeid'] = pd.Series(fakeids, index=df.index)
+    return df
+
+def makeFakes(csvFile):
+    detList = LL.wrapDetections(csvFile) 
+    tripDict = {}
+    for det in detList:
+        if(det.fakeid == -1):
+            continue
+        if(det.fakeid in tripDict):
+            tripDict[det.fakeid].dets.append(det)
+        else:
+            tripDict[det.fakeid] = Triplet([det])
+    tripList = []
+    for k in tripDict:
+        tripList.append(tripDict[k])
+    
+    writeTriplets(tripList, 'trueTriplets+' + seasonInfo + '.txt')
+    pickleTriplets(tripList, 'trueTriplets+' + seasonInfo + '.pickle')
+
+
+
 def main():
     args = argparse.ArgumentParser()
     args.add_argument('-t', '--triplets', help='pickle file of list of triplets')
     args.add_argument('-f', '--fakes', help='file of fakeids and objids')
+    args.add_argument('-c', '--detections', help='unblind detections of a patch')
+    args.add_argument('-u', '--truths', help='csvfile with fakeids')
     args = args.parse_args()
+    
+    if(args.truths):
+        makeFakes(args.truths)
+
+    if(args.detections):
+        fakeDict = makeFakeDict(args.fakes)
+        for patch in range(33, 100):
+            print('unblinding patch: ' + str(patch))
+            unblindPatch(fakeDict, patch)
+        return
+
 
     base = args.triplets.split('.')[0]
     if('/' in base):
